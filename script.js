dc551cea1771efd390568e7a0f1aefdd3258a693
Sam <98746718+Sam-L-White@users.noreply.github.com>
2023-03-14 15:52:38 +0000

    Add extra exercises

--- script.js
+++ script.js
@@ -19,9 +19,58 @@ function arithProgress(n){
 
 }
 
+function factorial(n){
+    if(n==1){
+        return n
+    } else {
+        return n * factorial(n-1)
+    }
+}
+
+function fibonacci(n){
+    if(n<=1){
+        return n
+    } else {
+        return fibonacci(n-1) + fibonacci(n-2)
+    }
+}
+
+let list = {
+    value: 1,
+    next: {
+      value: 2,
+      next: {
+        value: 3,
+        next: {
+          value: 4,
+          next: null
+        }
+      }
+    }
+};
+
+function printList(list){
+    console.log(list.value)
+    if(list.next){
+        printList(list.next)
+    }
+}
+
+function reverseList(list){
+    if(list.next){
+        reverseList(list.next);
+    }
+      alert(list.value);
+}
+
+printList(list)
+reverseList(list)
+
 let x = sumToLoop(10)
 let y = sumToRecursion(10)
 let z = arithProgress(10)
 console.log(x)
 console.log(y)
 console.log(z)
+console.log(factorial(20))
+console.log(fibonacci(3))
